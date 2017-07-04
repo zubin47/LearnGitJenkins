dea968168f1cab0406c015153701c6ca8eed889c
Zubin Bhattacharjee <t-bhatz>
2017-07-04 16:37:48 +0530

    Updated Cal_9

--- TestAutomation_Git_Jenkins/src/test_git_jenkins/Program.java
+++ TestAutomation_Git_Jenkins/src/test_git_jenkins/Program.java
@@ -14,6 +14,23 @@ public class Program
 		System.out.println("subtraction:" + (a - b));
 		System.out.println("division:" + (a/b));
 
+		Calc(47);
+		
+	}
+	
+	public static void Calc(int a)
+	{
+		System.out.println(a);
+		
+		a = a * 2;
+		a = a + 3;
+		a = a * 5;
+		a = a - 6;
+		
+		System.out.println(a);
+		
 	}
 
+	
+	
 }
