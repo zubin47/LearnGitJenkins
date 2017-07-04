3858f54247b6c959d8aedd96d38a64fafdf89e33
Zubin Bhattacharjee <t-bhatz>
2017-07-04 13:06:29 +0530

    Updated Multiplication code

--- TestAutomation_Git_Jenkins/src/test_git_jenkins/Program.java
+++ TestAutomation_Git_Jenkins/src/test_git_jenkins/Program.java
@@ -5,7 +5,11 @@ public class Program
 
 	public static void main(String[] args) 
 	{
+		int a = 8;
+		int b = 8;
+		
 		System.out.println("This is first code");
+		System.out.println("multiplication:" + (a*b));
 
 	}
 
